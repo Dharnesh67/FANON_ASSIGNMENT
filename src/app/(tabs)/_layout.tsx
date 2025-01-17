2a75dd6fbb3dc4e3c33d7ffdffbdbeb28ee183b2
Dharmesh <raikwardharmesh67@gmail.com>
2025-01-17 23:48:35 +0530

    edited Tabs

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,20 +1,33 @@
-import { View, Text } from 'react-native'
-import { FontAwesome } from '@expo/vector-icons'
-import React from 'react'
-import { Tabs } from 'expo-router'
+import { View, Text } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import React from "react";
+import { Tabs } from "expo-router";
 const TabsLayout = () => {
   return (
-  
-      <Tabs screenOptions={{tabBarActiveTintColor:'purple'}}>
-      <Tabs.Screen 
+    <Tabs screenOptions={{ tabBarActiveTintColor: "black", tabBarShowLabel: false,headerTitleStyle:{fontWeight:700 } }}>
+      <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+          title: "Home",
+          tabBarIcon: ({ color }) => <FontAwesome size={30} name="home" color={color} />,
         }}
       />
-      </Tabs>
-  )
-}
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: "Create Post",
+          tabBarIcon: ({ color }) => <FontAwesome size={30} name="plus-circle" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profilepage"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color }) => <FontAwesome size={30} name="user" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+};
 
-export default TabsLayout
+export default TabsLayout;
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 
 const index = () => {
   return (
-      <Text >Feed</Text>
+      <View style={{flex:1,justifyContent:'center',alignItems:'center'}} >
+        <Text>Feed</Text>
+      </View>
   )
 }
 
