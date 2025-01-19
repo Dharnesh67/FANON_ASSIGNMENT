bc6f42d6324f3667e6fced72032f703be9f0530b
Dharmesh <raikwardharmesh67@gmail.com>
2025-01-19 12:36:50 +0530

    Create.tsx Final

--- src/app/(tabs)/create.tsx
+++ src/app/(tabs)/create.tsx
@@ -1,31 +1,35 @@
-import React, { useState } from 'react';
-import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, Image, TextInput, Pressable } from "react-native";
 
 const Create = () => {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const image = "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg";
-
+  const [Title, setTitle] = useState("")
   const handlePost = () => {
     // Handle the post creation logic here
-    console.log('Post Created:', text);
-    setText(''); // Clear the input after posting
+    console.log("Post Created:", text);
+    setTitle(text);
+    setText(""); // Clear the input after posting
   };
 
   return (
     <View className="flex flex-1 items-center text-purple-500 p-2">
       {/* Image Picker */}
-      <Image 
-        source={{ uri: image }} 
-        className="w-52 aspect-[3/4] rounded-2xl bg-slate-200" 
-      />
-      
+      <Image source={{ uri: image }} className="w-52 aspect-[3/4] rounded-2xl bg-slate-200" />
+
       {/* Create Post Title */}
       <Text className="text-xl mt-4 text-blue-500">Create a post</Text>
-      
+
+      {
+        Title && <View className="flex flex-row items-center border-b border-gray-300 w-72 justify-between">
+          <Text className="text-xl mt-4 text-black">{Title}</Text> 
+        </View> 
+      }
+
       {/* Text Input */}
       <TextInput
         value={text}
-        onChangeText={setText}  // Update state with the input value
+        onChangeText={(v)=> setText(v)} // Update state with the input value
         placeholder="Write your post here..."
         className="w-72 mt-2 p-2 border border-gray-300 rounded-xl"
         multiline
@@ -33,12 +37,12 @@ const Create = () => {
       />
 
       {/* Button */}
-      <TouchableOpacity 
-        onPress={handlePost} 
-        className="mt-4 bg-blue-500 py-2 px-6 rounded-full"
-      >
+      <View className="mt-auto w-full">
+
+      <Pressable onPress={handlePost} className="mt-4 bg-blue-500 py-2 px-6 items-center  rounded-md">
         <Text className="text-white text-lg">Post</Text>
-      </TouchableOpacity>
+      </Pressable>
+      </View>
     </View>
   );
 };
